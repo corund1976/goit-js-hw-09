9e5bfb4cc88c25ff742938a2715ec28360533db5
Sergii Budak <corund1976@gmail.com>
2021-10-05 15:48:04 +0300

    Final Edition

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,22 +8,25 @@ let timerId = null;
 const buttonStartNode = document.querySelector('button[data-start]');
 const buttonStopNode = document.querySelector('button[data-stop]');
 
+buttonStartNode.disabled = false;
+
+const onStartClick = () => {
+    // buttonStartNode.removeEventListener('click', onStartClick);
+    buttonStartNode.disabled = true;
+    timerId = setInterval(() => {
+        const currentColor = getRandomHexColor();
+        document.body.style.backgroundColor = currentColor;
+    }, 1000);
+}    
+
 const onStopClick = () => {
+    // buttonStartNode.addEventListener('click', onStartClick);
+    buttonStartNode.disabled = false;
     clearInterval(timerId);
     document.body.style.backgroundColor = currentColor;
     buttonStartNode.addEventListener('click', onStartClick);
 };
 
-const onStartClick = () => {
-    buttonStartNode.removeEventListener('click', onStartClick);
-    buttonStopNode.addEventListener('click', onStopClick);
-    timerId = setInterval(() => {
-        const currentColor = getRandomHexColor();
-        document.body.style.backgroundColor = currentColor;
-    }, 1000);
-}
-
 buttonStartNode.addEventListener('click', onStartClick);
-    
-    
+buttonStopNode.addEventListener('click', onStopClick);
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,39 +1,27 @@
 // Импорт алерта
 import Notiflix from 'notiflix';
 
-function createPromise(position, delay) {
+function createPromise(position, delay){
   const shouldResolve = Math.random() > 0.3;
-  const obj = {
-    position: position,
-    delay: delay,
-  };
+  const obj = { position, delay };
 
-  if (shouldResolve) {
-    // Fulfill
-    const promise = new Promise((resolve, reject) => {
-      // Asynchronous operation
-      setTimeout((delay) => {
-        resolve(obj);
-      }, delay);
-    })
-    .then(({ position, delay }) => {
-        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-    })
-  } else {
-    // Reject
-    const promise = new Promise((resolve, reject) => {
-      // Asynchronous operation
-      setTimeout((delay) => {
-        reject(obj);
-      }, delay);
-    })
-    .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-    });
-  }
-}
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) resolve(obj);
+      else reject(obj);
+    }, delay);
+  })
+    
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+  })
+    
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  });
+};
 
-const inputForm = document.querySelector('.form')
+const inputForm = document.querySelector('.form');
 
 inputForm.addEventListener('submit', onSubmit);
 
@@ -49,5 +37,4 @@ function onSubmit(event) {
 
     delay = delay + step;
   }
-}
-
+};
